d9450a3a79d0f5edba5f377faa8b9e5aea20282a
Mohd Shayan Khan <61819556+msktechy@users.noreply.github.com>
2024-12-15 18:49:09 +0400

    Update LinkedIn-Auto-Send-Connection.js

--- LinkedIn-Auto-Send-Connection.js
+++ LinkedIn-Auto-Send-Connection.js
@@ -1,8 +1,8 @@
 Linkedin = {
   config: {
-    scrollDelay: 4000, // Delay for scrolling
-    actionDelay: 4000, // Delay for actions like clicking buttons
-    nextPageDelay: 4000, // Delay for moving to the next page
+    scrollDelay: 10000, // Delay for scrolling
+    actionDelay: 10000, // Delay for actions like clicking buttons
+    nextPageDelay: 10000, // Delay for moving to the next page
     maxRequests: -1, // Set to -1 for no limit
     totalRequestsSent: 0, // Count of total sent requests
     addNote: true, // Add a note with each connection request
